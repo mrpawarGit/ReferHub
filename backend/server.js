dd2af9ef00eebca37c331708d857c444549b8ffd
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2026-01-08 17:19:40 +0530

    basic server setup

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+// Test / Default Route
+app.get("/", (req, res) => {
+  res.json({ msg: "ReferHub API Running" });
+});
+
+// 404 Handle
+app.use((req, res) => {
+  res.status(404).json({ msg: "Not Found" });
+});
+
+// server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
