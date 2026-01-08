9a5f166aeec49c34e5c0abbbb6deeb3edc170389
mrpawarGit <154245119+mrpawarGit@users.noreply.github.com>
2026-01-08 18:22:58 +0530

    Register & Login - Done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -7,12 +7,12 @@ exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
-    // checkpoint - All fields are required
+    // check - All fields are required
     if (!name || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
 
-    // checkpoint - user exits or not
+    // check - user exits or not
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
@@ -32,6 +32,57 @@ exports.register = async (req, res) => {
     // sending res
     res.status(200).json({
       message: "User registered successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+// Login / SignIn
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check - All fields are required
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
+    }
+
+    // check - User If Exist
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // JWT
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    // sending res
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
     });
   } catch (error) {
     console.error(error);
